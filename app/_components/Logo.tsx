fe59695da657e5322b3a8229b0e5b8f63ce64554
Hanna Kulikowska <hannakulikowska.new@gmail.com>
2024-10-29 19:39:56 +0100

    refactor: optimize Image component

--- app/_components/Logo.tsx
+++ app/_components/Logo.tsx
@@ -1,10 +1,11 @@
 import Link from "next/link";
 import Image from "next/image";
+import logo from "@/public/logo.png";
 
 export default function Logo() {
   return (
     <Link href="/" className="flex items-center gap-4 z-10">
-      <Image src="/logo.png" width={60} height={60} alt="The Wild Oasis Logo" />
+      <Image src={logo} quality={100} width={60} height={60} alt="The Wild Oasis Logo" />
       <span className="text-xl font-semibold text-primary-100">
         The Wild Oasis
       </span>
